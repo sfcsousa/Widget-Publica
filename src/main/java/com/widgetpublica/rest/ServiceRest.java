c16784eff5993c558c1dbdd4dd9eead116a46aa6
pedro <pedromuriel.sousa@gmail.com>
2020-11-30 14:48:02 -0300

    teste

--- src/main/java/com/widgetpublica/rest/ServiceRest.java
+++ src/main/java/com/widgetpublica/rest/ServiceRest.java
@@ -63,118 +63,74 @@ public class ServiceRest extends WCMRest {
 
 		return Response.status(status).entity(response).type(MediaType.APPLICATION_JSON).build();
 	}
-
+	
 	@GET
-	@Path("layouts/{tenantId}")
+	@Path("/publicHello")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response helloPublic() {
+		String hello = "Hello World API!";
+		Integer status = 200;
+
+		Map<String, String> response = new HashMap<String, String>();
+		response.put("chave_1", hello);
+
+		return Response.status(status).entity(response).type(MediaType.APPLICATION_JSON).build();
+	}
+
+	@POST
+	@Path("/dataset")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response getLayouts(@PathParam("tenantId") Long tenantId) {
-		String APP_KEY = "9876-5432-1987-6543";
-		String UTF_8_ENCODE = StandardCharsets.UTF_8.name();
+	public Response getLayouts(String params) {
+		String APP_KEY = "415998177261c83957d08b85b49a98a6";//"9876-5432-1987-6543";
+
+		JSONParser parser = new JSONParser();	
 
 		try {
-			KeyVO key = Keyring.getKeys(tenantId, APP_KEY);
-			OAuthConsumer consumer = new DefaultOAuthConsumer(key.getConsumerKey(), key.getConsumerSecret());
-			consumer.setTokenWithSecret(key.getToken(), key.getTokenSecret());
 
-			URL url = new URL(key.getDomainUrl() + "/page-management/api/v2/layouts");
+			KeyVO key = Keyring.getKeys((long) 1, APP_KEY);
 
-			HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-			urlConnection.setRequestMethod("GET");
-			urlConnection.setDoInput(true);
-			urlConnection.setDoOutput(true);
-			consumer.sign(urlConnection);
-			urlConnection.connect();
+			JSONObject obj = (JSONObject) parser.parse(params);
 
-			Reader input = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), UTF_8_ENCODE));
-			String retLayout = "";
+			String datasetId = (String) obj.get("datasetId");
+			
+			OAuthConsumer config = config(key);
+			URL url = new URL(key.getDomainUrl() + "/api/public/ecm/dataset/datasets");
 
-			for (int c = input.read(); c != -1; c = input.read()) {
-				retLayout += (char) c;
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod(RestConstant.REQUEST_METHOD_POST);
+			conn.setRequestProperty("Accept-Charset", "UTF-8");
+			conn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+			conn.setDoInput(true);
+			conn.setDoOutput(true);
+			config.sign(conn);
+
+			String json = "{" + "\"name\": \""+datasetId+"\"," + "\"fields\": null," + "\"constraints\": null }";
+
+			OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+			wr.write(json);
+			wr.flush();
+			wr.close();
+
+			conn.connect();
+
+			Reader inputCreateUser = new BufferedReader(
+					new InputStreamReader(conn.getInputStream(), RestConstant.UTF_8_ENCODE));
+			String result = "";
+			for (int c = inputCreateUser.read(); c != -1; c = inputCreateUser.read()) {
+				result += (char) c;
 			}
+			int code = conn.getResponseCode();
+			System.out.println(String.format("RESPONSE: %d - %s: data: %s", code, conn.getResponseMessage(), result));
 
-			int code = urlConnection.getResponseCode();
-			urlConnection.disconnect();
-			return Response.status(code).entity(retLayout).build();
-
+			conn.disconnect();
+			return Response.status(code).entity(result).build();
 		} catch (Exception e) {
 			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
 		}
 	}
 
-	@POST
-	@Path("/appendPdfToHistory")
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Response postAppendPdfToHistory(String params) throws ParseException {
-
-		JSONParser parser = new JSONParser();
-		JSONObject obj = (JSONObject) parser.parse(params);
-
-		String base64 = (String) obj.get("documentBase64");
-		String historico = (String) obj.get("documentHistory");
-
-		try {
-
-			File file = new File("./baseFile.pdf");
-
-			FileOutputStream fos = new FileOutputStream(file);
-			// To be short I use a corrupted PDF string, so make sure to use a valid one if
-			// you want to preview the PDF file
-			byte[] decoder = Base64.getDecoder().decode(base64);
-			fos.write(decoder);
-
-			String template = historico;
-
-			HtmlConverter.convertToPdf(template, new FileOutputStream("./historico.pdf"));
-
-			List<InputStream> inputPdfList = new ArrayList<InputStream>();
-			inputPdfList.add(new FileInputStream("./baseFile.pdf"));
-			inputPdfList.add(new FileInputStream("./historico.pdf"));
-
-			// prepare output stream for merged pdf file.
-			OutputStream outputStream = new FileOutputStream("./docAssinado.pdf");
-
-			// call method to merge pdf files.
-			MergeFiles.mergePdfFiles(inputPdfList, outputStream);
-
-			System.out.println("PDF File Saved");
-
-			byte[] imageBytes = FileUtils.readFileToByteArray(new File("./docAssinado.pdf"));
-			String imageStr = Base64.getEncoder().encodeToString(imageBytes);
-
-			Map<String, String> response = new HashMap<String, String>();
-			response.put("docAssinado", imageStr);
-
-			File histFile = new File("./historico.pdf");
-			File docAssinado = new File("./docAssinado.pdf");
-			File baseFile = new File("./baseFile.pdf");
-
-			if (histFile.delete()) {
-				System.out.println("File deleted successfully");
-			} else {
-				System.out.println("Failed to delete the file");
-			}
-			if (docAssinado.delete()) {
-				System.out.println("File deleted successfully");
-			} else {
-				System.out.println("Failed to delete the file");
-			}
-
-			if (baseFile.delete()) {
-				System.out.println("File deleted successfully");
-			} else {
-				System.out.println("Failed to delete the file");
-			}
-			return Response.status(200).entity(response).type(MediaType.APPLICATION_JSON).build();
-
-		} catch (Exception e) {
-			System.out.println("Erro ao retornar o arquivo ");
-			e.printStackTrace();
-			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new ErrorStatus(e)).build();
-		}
-	}
-
+	
 	@POST
 	@Path("/iniciarProcessoSeletivo")
 	@Produces(MediaType.APPLICATION_JSON)
--- src/main/java/com/widgetpublica/wsdl/client/ECMDocumentService/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://ws.dm.ecm.technology.totvs.com/")
-package com.widgetpublica.wsdl.client.ECMDocumentService;
